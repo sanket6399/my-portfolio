f888742b7b9f59bc0d45d4eed8bbfc77672c0852
sanket6399 <sanket.kulkarni@sjsu.edu>
2023-12-22 16:53:05 -0800

    Updated the code file

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 
 import styles from './App.module.css';
-import { Navbar } from './components/navbar/navbar';
+import { Navbar } from './components/Navbar/Navbar';
 import { Hero } from './components/Hero/Hero';
 import { About } from './components/About/About';
 import { Experience} from './components/Experience/Experience';
